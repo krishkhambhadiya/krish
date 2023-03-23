70ff0ebafdf9ec6fe5c6893df673980ffe0d4a64
krish khambhadiya <khambhadiyakrish699@gmail.com>
2023-03-23 17:13:14 +0530

    filr upload successfully

---
+++ PROG2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+	int a=15;
+	int b=5;
+	int c=a-b;
+	printf("sum of c=%d");
+	getch();
+}
---
+++ PROG3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+	int a=5;
+	int b=10;
+	int c=a*b;
+	printf("sum of c=%d");
+	getch();
+}
